bdd322b0cf421223d9e46c3d766295d4190c79bc
hark130 <hark130@yahoo.com>
2023-08-28 17:22:04 -0500

    KERI-14: Cleaned up 8-04-1 test code
    Added better feedback on passes/failures
    Cleaned up some stray comments
    
    Changes to be committed:
    	modified:   code/src/8-04-1-hash_table-main.c
    
    Changes not staged for commit:
    	modified:   code/Makefile
    	modified:   code/src/8-04-hash_functions.c
    	modified:   code/src/8-04-hash_table-solution.c
    
    Untracked files:
    	code/src/8-04-2-hash_table-main.c

--- code/src/8-04-1-hash_table-main.c
+++ code/src/8-04-1-hash_table-main.c
@@ -3,6 +3,12 @@
 #include <string.h>  // strerror()
 
 
+#define GOOD_JOB "\xE2\x9C\x94"  // Check mark?
+#define BAD_JOB "X"              // Fail
+
+
+void _print_results(return_value result, const char *message);
+
 void _set_char(any_data_ptr data_struct, char *data);
 
 void _set_double(any_data_ptr data_struct, double *data);
@@ -12,42 +18,6 @@ void _set_float(any_data_ptr data_struct, float *data);
 void _set_int(any_data_ptr data_struct, int *data);
 
 void _set_string(any_data_ptr data_struct, char *data);
-
-// NORMAL
-// 1 Create & destroy
-// 2 find_value() empty table
-// 3 add_key() empty table
-// 4 add_key() a few entries
-// 5 add_key() a few entries, delete one
-// 6 add_key() a few entries, delete_all_keys()
-// 7 add_key() a few entries, delete_all_keys(), add_key() a few entries
-// 8 find_value() on an empty table
-// 9 find_value() on a non-empty table (key is missing)
-// 10 find_value() on a non-empty table (key is found)
-
-// ERROR
-// 1 create() invalid capacity
-// 2 create() NULL pointer
-// 3 find_value() NULL table
-// 4 find_value() NULL table->table_ptr
-// 5 find_value() NULL result
-// 6 add_key() NULL table
-// 7 add_key() NULL table->table_ptr
-// 8 add_key() NULL key
-// 9 add_key() NULL key->d_ptr
-// 10 add_key() 0 key->d_size
-// 11 add_key() NULL value
-// 12 add_key() NULL value->d_ptr
-// 13 add_key() 0 value->d_size
-// 14 delete_key() NULL table
-// 15 delete_key() NULL table->table_ptr
-// 16 delete_key() NULL key
-// 17 delete_key() NULL key->d_ptr
-// 18 delete_key() 0 key->d_size
-// 19 delete_all_keys() NULL table
-// 20 delete_all_keys() NULL table->table_ptr
-// 21 destroy_table() NULL table
-// 22 destroy_table() NULL table->table_ptr
 
 
 int main()
@@ -64,6 +34,10 @@ int main()
     float temp_float = 0;               // Temp var to use for any_data.d_ptr
     int temp_int1 = 0;                  // Temp var to use for any_data.d_ptr
     int temp_int2 = 0;                  // Temp var to use for any_data.d_ptr
+    unsigned int num_entries = 0;       // Store the number of table entries at run-time
+
+    // SETUP
+    fprintf(stdout, "Running tests from %s\n", __FILE__);
 
     // CREATE
     table = create_hash_table(start_cap, &result);
@@ -74,6 +48,7 @@ int main()
         {
             fprintf(stderr, "create_hash_table(): %s\n", strerror(result));
         }
+        _print_results(result, "Creating a hash table");
     }
 
     // ADD
@@ -84,10 +59,7 @@ int main()
         temp_int2 = 42;
         _set_int(&value, &temp_int2);
         result = add_key(table, &key, &value);
-        if (RET_SUCCESS != result)
-        {
-            fprintf(stderr, "The call to add_key() failed with: %d\n", result);
-        }
+        _print_results(result, "Adding a key");
     }
     // Key 2
     if (RET_SUCCESS == result)
@@ -97,10 +69,7 @@ int main()
         temp_int2 = 90;
         _set_int(&value, &temp_int2);
         result = add_key(table, &key, &value);
-        if (RET_SUCCESS != result)
-        {
-            fprintf(stderr, "The call to add_key() failed with: %d\n", result);
-        }
+        _print_results(result, "Adding a second key");
     }
     // Key 3
     if (RET_SUCCESS == result)
@@ -110,10 +79,7 @@ int main()
         temp_dble = 1/(double)7;
         _set_double(&value, &temp_dble);
         result = add_key(table, &key, &value);
-        if (RET_SUCCESS != result)
-        {
-            fprintf(stderr, "The call to add_key() failed with: %d\n", result);
-        }
+        _print_results(result, "Adding a third key");
     }
     // FIND (Key 2)
     if (RET_SUCCESS == result)
@@ -142,16 +108,22 @@ int main()
                 fprintf(stderr, "The value found doesn't match the orignal key's value");
             }
         }
+        _print_results(result, "Finding the second key");
     }
     // DELETE AND FIND (Key 3)
     if (RET_SUCCESS == result)
     {
+        num_entries = table->entries;  // Store the current number of entries
         _set_string(&key, "What is 6 x 7?");
         // Delete
         result = delete_key(table, &key);
         if (RET_SUCCESS != result)
         {
             fprintf(stderr, "Call to delete_key() failed to delete key %s\n", (char*)key.d_ptr);
+        }
+        else if (table->entries != num_entries - 1)
+        {
+            fprintf(stderr, "The tables entry counter did not decrement after delete_key()\n");
         }
         else
         {
@@ -169,6 +141,7 @@ int main()
                 result = RET_SUCCESS;  // NULL found value? 'Not found' result?  Perfect.
             }
         }
+        _print_results(result, "Deleting the third key");
     }
     // DELETE ALL ENTRIES
     if (RET_SUCCESS == result)
@@ -181,7 +154,9 @@ int main()
         else if (table->entries > 0)
         {
             fprintf(stderr, "How are there are still %d entries\n", table->entries);
+            result = RET_ERROR;  // Manually set
         }
+        _print_results(result, "Deleting all hash table entries");
     }
     // FIND (a needle in an empty haystack)
     if (RET_SUCCESS == result)
@@ -198,19 +173,36 @@ int main()
             fprintf(stderr, "The call to find_value() shoud have resulted in %d instead of %d\n",
                     RET_NOT_FOUND, result);
         }
+        else
+        {
+            result = RET_SUCCESS;  // Manually set
+        }
+        _print_results(result, "*NOT* finding a needle in an empty haystack");
     }
 
     // DONE
     if (table)
     {
         result = destroy_table(table);
-        if (result)
-        {
-            fprintf(stderr, "destroy_table() failed with %d\n", result);
-        }
+        _print_results(result, "Destroying a hash table");
         table = NULL;
     }
     return result;
+}
+
+
+void _print_results(return_value result, const char *message)
+{
+    // Success
+    if (RET_SUCCESS == result)
+    {
+        fprintf(stdout, "[%s] %s passed\n", GOOD_JOB, message);
+    }
+    else
+    {
+        fprintf(stderr, "[%s] %s <<<FAILED>>> with a return value of %d\n", BAD_JOB, message,
+                result);
+    }
 }
 
 
