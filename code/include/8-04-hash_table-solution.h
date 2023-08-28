0e929b645b34837892f59061cc201fe3470be12a
hark130 <hark130@yahoo.com>
2023-08-28 17:20:04 -0500

    KERI-14: Polished 8-04 library solution header
    Added another return value
    Extended one library API function comment block
    
    Changes to be committed:
    	modified:   code/include/8-04-hash_table-solution.h
    
    Changes not staged for commit:
    	modified:   code/Makefile
    	modified:   code/src/8-04-1-hash_table-main.c
    	modified:   code/src/8-04-hash_functions.c
    	modified:   code/src/8-04-hash_table-solution.c
    
    Untracked files:
    	code/src/8-04-2-hash_table-main.c

--- code/include/8-04-hash_table-solution.h
+++ code/include/8-04-hash_table-solution.h
@@ -4,10 +4,11 @@
 
 typedef enum _return_value
 {
-    RET_SUCCESS = 0,    // Success
-    RET_INV_PARAM = 1,  // Bad input: NULL pointer, unsupported data type
-    RET_ERROR = 2,      // System call failed (print errno)
-    RET_NOT_FOUND = 3,  // Key not found
+    RET_SUCCESS = 0,     // Success
+    RET_INV_PARAM = 1,   // Bad input: NULL pointer, unsupported data type
+    RET_ERROR = 2,       // System call failed (print errno)
+    RET_NOT_FOUND = 3,   // Key not found
+    RET_DUPE_ENTRY = 4,  // New entry already exists in hash table
 } return_value, *return_value_ptr;
 
 
@@ -68,8 +69,8 @@ any_data_ptr find_value(hash_table_ptr table, any_data_ptr key, return_value_ptr
 /*
  *  Allocates heap memory to copy the key and value.  Adds the copied pair to the hash table.
  *
- *  Returns RET_SUCCESS on success, RET_INV_PARAM for bad input, or RET_ERROR if an OS system call
- *  fails (and print errno).
+ *  Returns RET_SUCCESS on success, RET_INV_PARAM for bad input, RET_DUPE_ENTRY is key is already
+ *  in the hash table, or RET_ERROR if an OS system call fails (and print errno).
  *
  *  NOTE: The caller is responsible for any memory storing the table, key, and value.
  */
