d1189b211595dff090362e39ad7a907b9b1f08b4
hark130 <hark130@yahoo.com>
2023-08-28 17:23:45 -0500

    KERI-14: Refactored 8-04-2 example solution
    Refactored the library solution for hash collision support.
    
    FUN FACT: I had originally thought I was going to do linear probing but
    I settled on chaining instead.
    
    Changes to be committed:
    	modified:   code/src/8-04-hash_table-solution.c
    
    Changes not staged for commit:
    	modified:   code/Makefile
    	modified:   code/src/8-04-hash_functions.c
    
    Untracked files:
    	code/src/8-04-2-hash_table-main.c

--- code/src/8-04-hash_table-solution.c
+++ code/src/8-04-hash_table-solution.c
@@ -1,3 +1,17 @@
+/*
+ *  NOTE TO STUDENTS: This implementation is not perfect.
+ *      The Good:
+ *          - It works.
+ *          - ASAN and Valgrind both give this implementation a clean bill of health.
+ *      The Bad:
+ *          - It might be safe to remove some of the data validation from internal functions.
+ *      The Ugly:
+ *          - Some library-level operations could be improved merely by changing the order of
+ *            operations.  (E.g., Check for duplicate keys prior to memory allocation.)
+ *          - My "hash collision" solution, chaining, might be faster to insert at the head
+ *            instead of the tail.
+ */
+
 #include "8-04-hash_functions.h"
 #include "8-04-hash_table-solution.h"
 #include <errno.h>    // errno
@@ -20,10 +34,13 @@
 
 typedef struct _entry_pair
 {
-    any_data_ptr key;
-    any_data_ptr value;
+    any_data_ptr key;          // The entry's key
+    any_data_ptr value;        // The entry's value
+    struct _entry_pair *next;  // Next node in the linked list
 } entry_pair, *entry_pair_ptr;
 
+
+void _print_data(void *data, unsigned int size);
 
 
 /**************************************************************************************************/
@@ -41,9 +58,13 @@ return_value _add_key(hash_table_ptr table, any_data_ptr key, any_data_ptr value
 unsigned int _calc_index(void *d_ptr, unsigned int d_size, unsigned int capacity);
 
 /*
- *  Remove the given index from the table.  Free any entry found.
+ *  Remove old_node from the given index from the table.  Free the node after it was removed.
+ *
+ *  Returns RET_SUCCESS on success, RET_INV_PARAM for any NULL pointer or invalid state
+ *  (e.g. bad table), RET_ERROR on a failed system call, and RET_NOT_FOUND if old_node isn't
+ *  found at old_index.
  */
-return_value _clear_index(hash_table_ptr table, unsigned int old_index);
+return_value _clear_index(hash_table_ptr table, unsigned int old_index, entry_pair_ptr old_node);
 
 /*
  *  Compares all aspects of s1 data against s2, starting with the bookkeeping.  Immediately
@@ -117,6 +138,11 @@ any_data_ptr _find_value(hash_table_ptr table, void *raw_data, data_type raw_dat
  *  See result value for error reporting.
  */
 entry_pair_ptr _get_index(hash_table_ptr table, unsigned int index, return_value_ptr result);
+
+/*
+ *  Insert new_entry into table regardless of table's status.  Hash collision?  Handle it.
+ */
+return_value _shove_entry_in(hash_table_ptr table, entry_pair_ptr new_entry);
 
 /*
  *  Validates any_data structs on behalf of the library.
@@ -221,6 +247,15 @@ return_value add_key(hash_table_ptr table, any_data_ptr key, any_data_ptr value)
 
     // INPUT VALIDATION
     retval = _validate_table(table);
+    if (RET_SUCCESS == retval)
+    {
+        // Evaluate "Load Factor" here
+        if (table->capacity <= table->entries)
+        {
+            fprintf(stderr, "Table is full!  Resize!");  // PLACEHOLDER
+            retval = RET_ERROR;
+        }
+    }
     if (RET_SUCCESS == retval)
     {
         retval = _validate_any_data(key);
@@ -334,58 +369,66 @@ return_value _add_entry(hash_table_ptr table, entry_pair_ptr new_entry)
         temp_data_ptr = find_value(table, new_entry->key, &retval);
         if (temp_data_ptr)
         {
-            fprintf(stderr, "This value already exists: %p!\n", new_entry);
-            retval = RET_ERROR;
+            retval = RET_DUPE_ENTRY;
         }
         else if (RET_NOT_FOUND == retval)
         {
             retval = RET_SUCCESS;
         }
     }
-    // Hash collision?
+    // Shove it in
     if (RET_SUCCESS == retval)
     {
-        // 1. Get the data's index
-        index = _calc_index(new_entry->key->d_ptr, new_entry->key->d_size, table->capacity);
-        // 2. Find the data
-        if (BAD_INDEX != index)
-        {
-            temp_entry = _get_index(table, index, &retval);
-        }
-        if (temp_entry)
-        {
-            fprintf(stderr, "Hash collision detected for new entry: %p!\n", new_entry);
-            retval = RET_ERROR;
-        }
-        else if (RET_NOT_FOUND == retval)
-        {
-            retval = RET_SUCCESS;
-        }
+        retval = _shove_entry_in(table, new_entry);
     }
-    // Calculate the index
-    if (RET_SUCCESS == retval)
-    {
-        index = _calc_index(new_entry->key->d_ptr, new_entry->key->d_size, table->capacity);
-        if (ERROR_HASH == index)
-        {
-            HARKLE_ERROR(_calc_index, Returned a bad hash value);
-            retval = RET_ERROR;
-        }
-    }
-    // Insert new_entry into table->table_ptr
-    if (RET_SUCCESS == retval)
-    {
-        if (index < table->capacity)
-        {
-            (*((entry_pair_ptr*)(table->table_ptr) + index)) = new_entry;
-            table->entries += 1;
-        }
-        else
-        {
-            HARKLE_ERROR(_add_entry, Detected an out of bounds index);
-            retval = RET_ERROR;  // Invalid index
-        }
-    }
+    // // Hash collision?
+    // if (RET_SUCCESS == retval)
+    // {
+    //     // 1. Get the data's index
+    //     index = _calc_index(new_entry->key->d_ptr, new_entry->key->d_size, table->capacity);
+    //     // 2. Find the data
+    //     if (BAD_INDEX != index)
+    //     {
+    //         temp_entry = _get_index(table, index, &retval);
+    //     }
+    //     if (temp_entry)
+    //     {
+    //         fprintf(stderr, "Hash collision detected for new entry: %p!\n", new_entry);
+    //         // fprintf(stderr, "Hash collision detected for new entry(key:value): %p(%p:%p)!\n",
+    //         //         new_entry, new_entry->key, new_entry->value);  // DEBUGGING
+    //         retval = RET_ERROR;
+    //     }
+    //     else if (RET_NOT_FOUND == retval)
+    //     {
+    //         retval = RET_SUCCESS;
+    //     }
+    // }
+    // // Calculate the index
+    // if (RET_SUCCESS == retval)
+    // {
+    //     index = _calc_index(new_entry->key->d_ptr, new_entry->key->d_size, table->capacity);
+    //     if (ERROR_HASH == index)
+    //     {
+    //         HARKLE_ERROR(_calc_index, Returned a bad hash value);
+    //         retval = RET_ERROR;
+    //     }
+    // }
+    // // Insert new_entry into table->table_ptr
+    // if (RET_SUCCESS == retval)
+    // {
+    //     if (index < table->capacity)
+    //     {
+    //         fprintf(stderr, "STORING %p(%p:%p) at index %d\n", new_entry, new_entry->key,
+    //                 new_entry->value, index);  // DEBUGGING
+    //         (*((entry_pair_ptr*)(table->table_ptr) + index)) = new_entry;
+    //         table->entries += 1;
+    //     }
+    //     else
+    //     {
+    //         HARKLE_ERROR(_add_entry, Detected an out of bounds index);
+    //         retval = RET_ERROR;  // Invalid index
+    //     }
+    // }
 
     // RESIZE
     // If we're going to resize the hash table at a certain "load factor" threshold, we do it here
@@ -420,11 +463,13 @@ return_value _add_key(hash_table_ptr table, any_data_ptr key, any_data_ptr value
     if (RET_SUCCESS == retval)
     {
         new_entry->key = _copy_any_data(key, &retval);
+        // fprintf(stderr, "COPIED %p to %p\n", key, new_entry->key);  // DEBUGGING
     }
     // Value
     if (RET_SUCCESS == retval)
     {
         new_entry->value = _copy_any_data(value, &retval);
+        // fprintf(stderr, "COPIED %p to %p\n", value, new_entry->value);  // DEBUGGING
     }
 
     // ADD TO HASH TABLE
@@ -476,6 +521,7 @@ unsigned long _calc_hash(void *d_ptr, unsigned int d_size)
 
     // GET IT
     hash = get_fnv_hash(d_ptr, d_size);
+    // fprintf(stderr, "HASH OF %p is %lu\n", d_ptr, hash);  // DEBUGGING
 
     // DONE
     return hash;
@@ -504,11 +550,12 @@ unsigned int _calc_index(void *d_ptr, unsigned int d_size, unsigned int capacity
 }
 
 
-return_value _clear_index(hash_table_ptr table, unsigned int old_index)
+return_value _clear_index(hash_table_ptr table, unsigned int old_index, entry_pair_ptr old_node)
 {
     // LOCAL VARIABLES
     return_value retval = RET_SUCCESS;     // Function call results
-    entry_pair_ptr old_entry = NULL;       // Entry to clear
+    entry_pair_ptr tmp_head = NULL;        // Head node found at old_index
+    entry_pair_ptr tmp_node = NULL;        // Temp node var to parse linked list
     entry_pair_ptr *table_entries = NULL;  // Pointer to the hash table array of entry pointers
 
     // INPUT VALIDATION
@@ -517,24 +564,75 @@ return_value _clear_index(hash_table_ptr table, unsigned int old_index)
     {
         retval = RET_INV_PARAM;
     }
+    if (RET_SUCCESS == retval)
+    {
+        retval = _validate_entry_pair(old_node);
+    }
 
     // CLEAR IT
+    // Get the head node
     if (RET_SUCCESS == retval)
     {
         table_entries = (entry_pair_ptr*)table->table_ptr;
-        old_entry = (*(table_entries + old_index));
-        if (!old_entry)
+        tmp_head = (*(table_entries + old_index));
+        if (!tmp_head)
         {
             retval = RET_NOT_FOUND;
         }
+        // else
+        // {
+        //     tmp_node = tmp_head;
+        //     // retval = _destroy_entry_pair(old_entry);
+        //     // if (RET_SUCCESS == retval)
+        //     // {
+        //     //     (*(table_entries + old_index)) = NULL;
+        //     // }
+        // }
         else
         {
-            retval = _destroy_entry_pair(old_entry);
-            if (RET_SUCCESS == retval)
+            retval = _validate_entry_pair(tmp_head);
+        }
+    }
+    // Update linked list
+    if (RET_SUCCESS == retval)
+    {
+        if (tmp_head == old_node)
+        {
+            (*(table_entries + old_index)) = tmp_head->next;
+        }
+        else
+        {
+            tmp_node = tmp_head->next;
+            while (tmp_node)
             {
-                (*(table_entries + old_index)) = NULL;
+                retval = _validate_entry_pair(tmp_node);
+                if (RET_SUCCESS == retval)
+                {
+                    if (tmp_node == old_node)
+                    {
+                        tmp_head->next = tmp_node->next;  // Remove it from the linked list
+                        break;  // Found it
+                    }
+                    else
+                    {
+                        retval = RET_NOT_FOUND;  // Haven't found it yet
+                        tmp_head = tmp_node;
+                        tmp_node = tmp_node->next;  // Move it on down the list
+                    }
+                }
+                else
+                {
+                    break;  // No need to continue
+                }
             }
         }
+    }
+    // Destroy it
+    if (RET_SUCCESS == retval)
+    {
+        old_node->next = NULL;  // Don't want to remove *all the nodes, just this one
+        retval = _destroy_entry_pair(old_node);
+        table->entries -= 1;
     }
 
     // DONE
@@ -741,31 +839,31 @@ return_value _delete_key(hash_table_ptr table, any_data_ptr key)
     return_value retval = RET_SUCCESS;  // Function call results
     unsigned int index = 0;             // Calculated index
     entry_pair_ptr old_entry = NULL;    // Entry to delete(?)
+    entry_pair_ptr old_head = NULL;     // Head entry
 
     // DELETE IT
     // Calc index
     index = _calc_index(key->d_ptr, key->d_size, table->capacity);
     if (BAD_INDEX != index)
     {
-        old_entry = _get_index(table, index, &retval);
+        old_head = _get_index(table, index, &retval);
+        old_entry = old_head;
     }
-    // Validate finding
-    if (RET_SUCCESS == retval)
+    while(old_entry)
     {
+        // Validate finding
         retval = _validate_entry_pair(old_entry);
-    }
-    // Verify a match
-    if (RET_SUCCESS == retval)
-    {
-        if (false == _compare_any_data(key, old_entry->key, &retval))
+        // Verify a match
+        if (RET_SUCCESS == retval && true == _compare_any_data(key, old_entry->key, &retval))
         {
-            retval = RET_NOT_FOUND;  // Wrong key
+            // Clear index
+            retval = _clear_index(table, index, old_entry);
+            break;
         }
-    }
-    // Clear index
-    if (RET_SUCCESS == retval)
-    {
-        retval = _clear_index(table, index);
+        else
+        {
+            old_entry = old_entry->next;
+        }
     }
 
     // DONE
@@ -861,6 +959,15 @@ return_value _destroy_entry_pair(entry_pair_ptr old_entry)
     // DESTROY IT
     if (RET_SUCCESS == retval)
     {
+        if (old_entry->next)
+        {
+            temp_ret = _destroy_entry_pair(old_entry->next);
+            if (temp_ret != RET_SUCCESS)
+            {
+                retval = temp_ret;
+            }
+            old_entry->next = NULL;
+        }
         if (old_entry->key)
         {
             temp_ret = _destroy_any_data(old_entry->key);
@@ -908,7 +1015,7 @@ return_value _destroy_table(hash_table_ptr old_table)
             temp_entry = (*(table_arr + i));
             if (temp_entry)
             {
-                fprintf(stderr, "Destroying %p\n", temp_entry);
+                // fprintf(stderr, "Destroying %p\n", temp_entry);  // DEBUGGING
                 temp_ret = _destroy_entry_pair(temp_entry);
                 if (temp_ret != RET_SUCCESS)
                 {
@@ -1014,12 +1121,30 @@ any_data_ptr _find_value(hash_table_ptr table, void *raw_data, data_type raw_dat
     // 3. Verify the data
     if (entry && RET_SUCCESS == retval)
     {
-        if (true == _compare_data(raw_data, raw_data_type, raw_data_size, entry->key, &retval))
+        while (entry)
         {
-            if (RET_SUCCESS == retval)
+            if (true == _compare_data(raw_data, raw_data_type, raw_data_size, entry->key, &retval))
             {
-                value = entry->value;
+                if (RET_SUCCESS == retval)
+                {
+                    // _print_data(raw_data, raw_data_size);  // DEBUGGING
+                    // _print_data(entry->key->d_ptr, entry->key->d_size);  // DEBUGGING
+                    value = entry->value;
+                    break;  // Found it
+                }
             }
+            else if (RET_SUCCESS != retval)
+            {
+                break;
+            }
+            else
+            {
+                entry = entry->next;  // Check the next node in the linked list
+            }
+        }
+        if (!value)
+        {
+            retval = RET_NOT_FOUND;  // Apparently, it wasn't found
         }
     }
 
@@ -1061,6 +1186,65 @@ entry_pair_ptr _get_index(hash_table_ptr table, unsigned int index, return_value
         *result = retval;
     }
     return entry;
+}
+
+
+return_value _shove_entry_in(hash_table_ptr table, entry_pair_ptr new_entry)
+{
+    // LOCAL VARIABLES
+    return_value retval = RET_SUCCESS;  // Function call results
+    unsigned int index = 0;             // new_entry->value index, based on the hash
+    any_data_ptr temp_data_ptr = NULL;  // Temp any_data_ptr for detecting collisions
+    entry_pair_ptr temp_entry = NULL;   // Temp entry var used for probing
+
+    // SHOVE IT IN
+    // Hash collision?
+    if (RET_SUCCESS == retval)
+    {
+        // 1. Get the data's index
+        index = _calc_index(new_entry->key->d_ptr, new_entry->key->d_size, table->capacity);
+        // 2. Find the data
+        if (BAD_INDEX != index)
+        {
+            temp_entry = _get_index(table, index, &retval);
+        }
+        else
+        {
+            retval = RET_ERROR;  // _calc_index() appears to have failed (somehow)
+        }
+        // 3. Insert it
+        // Hash collision detected
+        if (RET_SUCCESS == retval && temp_entry)
+        {
+            // fprintf(stderr, "Hash collision detected for: %p!\n", new_entry);  // DEBUGGING
+            while (NULL != temp_entry->next)
+            {
+                temp_entry = temp_entry->next;  // Find the end of the linked list
+            }
+            temp_entry->next = new_entry;
+            table->entries += 1;
+        }
+        // Insert as normal
+        else if (RET_NOT_FOUND == retval)
+        {
+            retval = RET_SUCCESS;
+            if (index < table->capacity)
+            {
+                // fprintf(stderr, "STORING %p(%p:%p) at index %d\n", new_entry, new_entry->key,
+                //         new_entry->value, index);  // DEBUGGING
+                (*((entry_pair_ptr*)(table->table_ptr) + index)) = new_entry;
+                table->entries += 1;
+            }
+            else
+            {
+                HARKLE_ERROR(_add_entry, Detected an out of bounds index);
+                retval = RET_ERROR;  // Invalid index
+            }
+        }
+    }
+
+    // DONE
+    return retval;
 }
 
 
