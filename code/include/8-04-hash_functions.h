4d6eb44dc86c1026cae596f61d12b2995f4ed348
hark130 <hark130@yahoo.com>
2023-08-29 17:54:35 -0500

    KERI-14: Added hash functions header comment block
    Changes to be committed:
    	modified:   code/include/8-04-hash_functions.h
    
    Changes not staged for commit:
    	modified:   code/src/8-04-1-hash_table-main.c
    	modified:   code/src/8-04-2-hash_table-main.c
    	modified:   code/src/8-04-3-hash_table-main.c
    	modified:   code/src/8-04-hash_table-solution.c
    
    Untracked files:
    	code/src/8-04-hash_table-lab.c

--- code/include/8-04-hash_functions.h
+++ code/include/8-04-hash_functions.h
@@ -1,3 +1,12 @@
+/*
+ *  This header file defines the two different hash functions for use by the 8-04 student labs.
+ *  Hash tables require hash functions but the 8.4 JQS line item doesn't explicitly require
+ *  developers write their own.
+ *
+ *  All things being equal, get_fnv_hash() is preferable but get_jank_hash() could help when
+ *  testing how well a hash table handles hash collisions.
+ */
+
 #ifndef _8_04_HASH_FUNCTIONS_
 #define _8_04_HASH_FUNCTIONS_
 
